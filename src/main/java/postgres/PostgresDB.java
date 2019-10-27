ee2c89c7eea928bd192698cf438af46c0b0e851f
leerigra <conoa@miobox.jp>
2019-10-27 13:17:49 +0900

    edit

--- src/main/java/postgres/PostgresDB.java
+++ src/main/java/postgres/PostgresDB.java
@@ -37,8 +37,7 @@ public class PostgresDB {
      */
     public PostgresDB() {
         driver = "org.postgresql.Driver";
-        url = "jdbc:postgres://irllxnufnkpwoh:fed0be0c6c2e1e1f1c5fa4253a70fe444e8a0fafbdbcd33d737ae82320ceba72@ec2-54-235-163-246.compute-1.amazonaws.com:5432/d7bt721ft6mc8e";
-       
+        url = "jdbc:postgresql://ec2-54-235-163-246.compute-1.amazonaws.com:5432/d7bt721ft6mc8e";
         user = "irllxnufnkpwoh";
         password = "fed0be0c6c2e1e1f1c5fa4253a70fe444e8a0fafbdbcd33d737ae82320ceba72";
 
